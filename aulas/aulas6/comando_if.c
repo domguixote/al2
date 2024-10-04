7f93e9d1fbe455eba33b019887015d2a1291553c
domguixote <guimirands10@gmail.com>
2024-10-04 22:17:06 +0000

    edit aula 6

--- aulas/aulas6/comando_if.c
+++ aulas/aulas6/comando_if.c
@@ -9,6 +9,7 @@ int main() {
     int eh_divisivel_por_2 = numero %2 == 0;
     
     //comando if completo
+    
     if(eh_divisivel_por_2) {
         printf("O numero %i eh par!\n", numero);
 
@@ -19,14 +20,16 @@ int main() {
     int idade = 0;
     printf("Entre com sua idade: ");
     deu_certo = scanf("%i", &idade);
+    
     //comando if aninhado
+    
     if (idade < 16) {
         printf("Você não pode votar!\n");
     } else {
         if (idade >= 18 && idade <= 70) {
-            printf("Você é obrigado a votar");
+            printf("Você é obrigado a votar\n");
         } else {
-            printf("Você não é obrigado a votar");
+            printf("Você não é obrigado a votar\n");
         }    
     }
 
@@ -35,25 +38,20 @@ int main() {
     deu_certo = scanf("%i", &media);
 
     //if encadeado
-    if (media >= 9.0f && media = <= 10.0f) {
+    
+    if (media >= 9.0f && media <= 10.0f) {
         printf("Sua mencao eh SS\n");
-    } else if (media >= 7.0f && media = <= 8.9f) {
+    } else if (media >= 7.0f && media <= 8.9f) {
         printf("Sua mencao eh MS\n");
-    } else if (media >= 5.0f && media = <= 6.9f) {
+    } else if (media >= 5.0f && media <= 6.9f) {
         printf("Sua mencao eh MM\n");
-    } else if (media >= 3.0f && media = <= 4.9f) {
+    } else if (media >= 3.0f && media <= 4.9f) {
         printf("Sua mencao eh MI\n");
-    } else if (media >= 0.1f && media = <= 2.9f) {
+    } else if (media >= 0.1f && media <= 2.9f) {
         printf("Sua mencao eh II\n");
     } else {
         printf("Sua mencao eh SR\n");
     }
-    
-    
-    
-    
-
-    
     
     return 0;
 }
